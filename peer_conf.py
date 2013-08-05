b5ba5fcb7df398a17314a281ddf0fea87ab63f20
mk7-555 <madhusudhanank@gmail.com>
2013-08-05 13:30:19 -0700

    New file added: peer_conf.py will maintain all parameters related to test peers.

---
+++ peer_conf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+mgmt_ip="10.192.46.152"
+user="root"
+passwd="4Emulex"
+driv_path="/root/evt/drivers/"
+driv_ver="10.0.563.0"
+num_ports=2
+
+peer_list_1port = ["194.24.15.210"]
+peer_list_2port = ["194.24.8.210", "194.24.9.211"]
+peer_list_4port = ["194.24.8.210", "194.24.9.211", "194.24.6.31", "194.24.7.32"]
+
+vlan_peer_1port = ["194.24.16.210"]
+vlan_peer_2port = ["194.24.11.100", "194.24.12.100"]
+vlan_peer_4port = ["194.24.11.100", "194.24.12.100", "194.24.13.100", "194.24.14.100"]
